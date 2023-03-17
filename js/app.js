48c528ac8983d3f76970b5659189d972bdde96e5
Arif Hossan <arif.hossan.eu@gmail.com>
2023-03-18 01:43:13 +0600

    image fetching solved

--- js/app.js
+++ js/app.js
@@ -11,15 +11,16 @@ const displayPhones = (phones, dataLimit) =>{
 
     // display 10 phones only
     const showAll = document.getElementById('show-all');
+
     if(dataLimit && phones.length > 10) {
+
         phones = phones.slice(0, 10);
-        // console.log(phones);
+
         showAll.classList.remove('d-none');
     }
     else{
         showAll.classList.add('d-none');
     }
-
 
     // display no phones found
     const noPhone = document.getElementById('no-found-message');
@@ -32,11 +33,12 @@ const displayPhones = (phones, dataLimit) =>{
     // display all phones
     phones.forEach(phone =>{
 
+        console.log(phone);
         const phoneDiv  = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
         <div class="card p-4">
-            <img src="${phone.images}" class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -88,7 +90,7 @@ document.getElementById('btn-show-all').addEventListener('click', function(){
 })
 
 const loadPhoneDetails = async id =>{
-    const url =`www.openapi.programming-hero.com/api/phone/${id}`;
+    const url =`https://www.openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
     displayPhoneDetails(data.data);
